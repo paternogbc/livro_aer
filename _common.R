5f650ffd5989087a041bdcb41cfdc3c84cdd8ee3
Gustavo Paterno <paternogbc@gmail.com>
2021-08-12 10:48:40 +0200

    add cap 05.

---
+++ _common.R
@@ -0,0 +1,21 @@
+set.seed(1014)
+
+knitr::opts_chunk$set(
+  comment = "#>",
+  collapse = TRUE,
+  cache = TRUE,
+  out.width = "70%",
+  fig.align = 'center',
+  fig.width = 6,
+  fig.asp = 0.618,  # 1 / phi
+  fig.show = "hold"
+)
+
+options(dplyr.print_min = 6, dplyr.print_max = 6)
+
+# Activate crayon output
+options(
+  crayon.enabled = TRUE,
+  pillar.bold = TRUE,
+  stringr.html = FALSE
+)
