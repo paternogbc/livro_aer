eb7bfc5b891705712ab0be9c78da652cde863019
seu nome <mauricio.vancine@gmail.com>
2022-02-05 13:08:38 -0300

    pos editora

--- _render_book.R
+++ _render_book.R
@@ -6,10 +6,10 @@ library(knitr)
 library(tictoc)
 
 # Clean files -------------------------------------------------------------
-# 
-# options(bookdown.clean_book = TRUE)
-# bookdown::clean_book()
-# rmarkdown::clean_site(preview = FALSE)
+
+options(bookdown.clean_book = TRUE)
+bookdown::clean_book()
+rmarkdown::clean_site(preview = FALSE)
 
 # Update ecodados
 remotes::install_github(repo = "paternogbc/ecodados", force = TRUE)
@@ -19,12 +19,14 @@ tic()
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 toc()
 
-# Render word
-# tic()
-# rmarkdown::render_site(output_format = 'bookdown::pdf_document2', encoding = 'UTF-8')
-# toc()
+# Render pdf
+tic()
+rmarkdown::render_site(output_format = 'bookdown::pdf_document2', encoding = 'UTF-8')
+toc()
 
 # Render word
+tic()
 rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
+toc()
 
 # end ---------------------------------------------------------------------
