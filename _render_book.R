229a180f033c4a0d6f8026f5b384ec38fc988e92
seu nome <mauricio.vancine@gmail.com>
2022-02-08 14:48:43 -0300

    updates exercicios

--- _render_book.R
+++ _render_book.R
@@ -12,21 +12,21 @@ bookdown::clean_book()
 rmarkdown::clean_site(preview = FALSE)
 
 # Update ecodados
-remotes::install_github(repo = "paternogbc/ecodados", force = TRUE)
+# remotes::install_github(repo = "paternogbc/ecodados", force = TRUE)
 
 # Render html -------------------------------------------------------------
 tic()
 rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
 toc()
 
-# # Render pdf
-# tic()
-# rmarkdown::render_site(output_format = 'bookdown::pdf_document2', encoding = 'UTF-8')
-# toc()
-# 
-# # Render word
-# tic()
-# rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
-# toc()
+# Render pdf
+tic()
+rmarkdown::render_site(output_format = 'bookdown::pdf_document2', encoding = 'UTF-8')
+toc()
+
+# Render word
+tic()
+rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
+toc()
 
 # end ---------------------------------------------------------------------
