5fb473d2418c7208e18572bf01052b096ccb4394
mauriciovancine <mauricio.vancine@gmail.com>
2022-02-14 15:34:28 -0300

    render bs4

--- _render_book.R
+++ _render_book.R
@@ -12,12 +12,12 @@ bookdown::clean_book()
 rmarkdown::clean_site(preview = FALSE)
 
 # Update ecodados
-remotes::install_github(repo = "paternogbc/ecodados", force = TRUE)
+# remotes::install_github(repo = "paternogbc/ecodados", force = TRUE)
 
 # Render html -------------------------------------------------------------
-tic()
-rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
-toc()
+# tic()
+# rmarkdown::render_site(output_format = 'bookdown::gitbook', encoding = 'UTF-8')
+# toc()
 
 # Render BS4 -------------------------------------------------------------
 tic()
@@ -30,9 +30,9 @@ toc()
 # toc()
 
 # Render word
-tic()
-rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
-toc()
+# tic()
+# rmarkdown::render_site(output_format = 'bookdown::word_document2', encoding = 'UTF-8')
+# toc()
 
 
 # end ---------------------------------------------------------------------
