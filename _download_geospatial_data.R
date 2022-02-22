36fe1336edb990efd38b5a53bedf2cbf18657248
mauriciovancine <mauricio.vancine@gmail.com>
2022-02-22 11:07:54 -0300

    prefacio e script download geoespacial

--- _download_geospatial_data.R
+++ _download_geospatial_data.R
@@ -20,34 +20,34 @@ dir.create(here::here("dados", "vetor"))
 
 ## Download
 for(i in c(".dbf", ".prj", ".shp", ".shx")){
-  
-  # Pontos de nascentes
-  download.file(
-    url = paste0("http://geo.fbds.org.br/SP/RIO_CLARO/HIDROGRAFIA/SP_3543907_NASCENTES", i),
-    destfile = here::here("dados", "vetor", paste0("SP_3543907_NASCENTES", i)), mode = "wb")
-  
-  # Linhas de hidrografia
-  download.file(
-    url = paste0("http://geo.fbds.org.br/SP/RIO_CLARO/HIDROGRAFIA/SP_3543907_RIOS_SIMPLES", i),
-    destfile = here::here("dados", "vetor", paste0("SP_3543907_RIOS_SIMPLES", i)), mode = "wb")
-  
-  # Polígonos de cobertura da terra
-  download.file(
-    url = paste0("http://geo.fbds.org.br/SP/RIO_CLARO/USO/SP_3543907_USO", i),
-    destfile = here::here("dados", "vetor", paste0("SP_3543907_USO", i)), mode = "wb")
+    
+    # Pontos de nascentes
+    download.file(
+        url = paste0("http://geo.fbds.org.br/SP/RIO_CLARO/HIDROGRAFIA/SP_3543907_NASCENTES", i),
+        destfile = here::here("dados", "vetor", paste0("SP_3543907_NASCENTES", i)), mode = "wb")
+    
+    # Linhas de hidrografia
+    download.file(
+        url = paste0("http://geo.fbds.org.br/SP/RIO_CLARO/HIDROGRAFIA/SP_3543907_RIOS_SIMPLES", i),
+        destfile = here::here("dados", "vetor", paste0("SP_3543907_RIOS_SIMPLES", i)), mode = "wb")
+    
+    # Polígonos de cobertura da terra
+    download.file(
+        url = paste0("http://geo.fbds.org.br/SP/RIO_CLARO/USO/SP_3543907_USO", i),
+        destfile = here::here("dados", "vetor", paste0("SP_3543907_USO", i)), mode = "wb")
 }
 
 ## Importar nascentes
 geo_vetor_nascentes <- sf::st_read(
-  here::here("dados", "vetor", "SP_3543907_NASCENTES.shp"), quiet = TRUE)
+    here::here("dados", "vetor", "SP_3543907_NASCENTES.shp"), quiet = TRUE)
 
 ## Importar hidrografia
 geo_vetor_hidrografia <- sf::st_read(
-  here::here("dados", "vetor", "SP_3543907_RIOS_SIMPLES.shp"), quiet = TRUE)
+    here::here("dados", "vetor", "SP_3543907_RIOS_SIMPLES.shp"), quiet = TRUE)
 
 ## Importar cobertura da terra
 geo_vetor_cobertura <- sf::st_read(
-  here::here("dados", "vetor", "SP_3543907_USO.shp"), quiet = TRUE)
+    here::here("dados", "vetor", "SP_3543907_USO.shp"), quiet = TRUE)
 
 ## Polígono do limite do município de Rio Claro
 geo_vetor_rio_claro <- geobr::read_municipality(code_muni = 3543907, 
@@ -70,8 +70,8 @@ unzip(zipfile = here::here("dados", "tabelas", "atlantic_amphibians.zip"),
 
 ## Importar tabela de locais
 geo_anfibios_locais <- readr::read_csv(
-  here::here("dados", "tabelas", "ATLANTIC_AMPHIBIANS_sites.csv"),
-  locale = locale(encoding = "Latin1"))
+    here::here("dados", "tabelas", "ATLANTIC_AMPHIBIANS_sites.csv"),
+    locale = locale(encoding = "Latin1"))
 geo_anfibios_locais
 
 ## Aumentar o tempo de download
@@ -102,96 +102,88 @@ unzip(zipfile = here::here("dados", "raster", "wc2.0_10m_bio.zip"),
 
 ## Listar arquivos
 arquivos_raster <- dir(path = here::here("dados", "raster"), pattern = "wc") %>% 
-  grep(".tif", ., value = TRUE)
+    grep(".tif", ., value = TRUE)
 arquivos_raster
+
+## Importar vários rasters como stack
+geo_raster_bioclim <- raster::stack(here::here("dados", "raster", arquivos_raster))
+geo_raster_bioclim
 
 ## Download de polígonos dos geo_vetor_biomas Brasileiros
 geo_vetor_biomas <- geobr::read_biomes(showProgress = FALSE) %>%
-  dplyr::filter(name_biome != "Sistema Costeiro")
+    dplyr::filter(name_biome != "Sistema Costeiro")
 
 ## Dados
 geo_vetor_am_sul <- rnaturalearth::ne_countries(continent = "South America")
 geo_vetor_brasil <- rnaturalearth::ne_countries(country = "Brazil")
 geo_vetor_biomas <- geobr::read_biomes(showProgress = FALSE) %>%
-  dplyr::filter(name_biome != "Sistema Costeiro")
+    dplyr::filter(name_biome != "Sistema Costeiro")
 
 ## Dados
 geo_vetor_brasil_anos <- NULL
 for(i in c(1872, 1900, 1911, 1920, 1933, 1940, 1950, 1960, 1970,
            1980, 1991, 2001, 2010, 2019, 2020)){
-  geo_vetor_brasil_anos <- geobr::read_state(code_state = "all",
-                                             year = i, showProgress = FALSE) %>% 
-    sf::st_geometry() %>% 
-    sf::st_as_sf() %>%
-    dplyr::mutate(year = i) %>% 
-    dplyr::bind_rows(geo_vetor_brasil_anos, .)
+    geo_vetor_brasil_anos <- geobr::read_state(code_state = "all",
+                                               year = i, showProgress = FALSE) %>% 
+        sf::st_geometry() %>% 
+        sf::st_as_sf() %>%
+        dplyr::mutate(year = i) %>% 
+        dplyr::bind_rows(geo_vetor_brasil_anos, .)
 }
 
 ## Importar locais
 geo_anfibios_locais <- readr::read_csv(
-  here::here("dados", "tabelas", "ATLANTIC_AMPHIBIANS_sites.csv"),
-  col_types = cols()) %>%
-  dplyr::select(id, longitude, latitude, species_number)
+    here::here("dados", "tabelas", "ATLANTIC_AMPHIBIANS_sites.csv"),
+    col_types = cols()) %>%
+    dplyr::select(id, longitude, latitude, species_number)
 
 ## Importar espécies
 geo_anfibios_especies <- readr::read_csv(
-  here::here("dados", "tabelas", "ATLANTIC_AMPHIBIANS_species.csv"), 
-  col_types = cols()) %>%
-  tidyr::drop_na(valid_name) %>% 
-  dplyr::select(id, valid_name, individuals) %>% 
-  dplyr::distinct(id, valid_name, .keep_all = TRUE) %>% 
-  dplyr::mutate(individuals = tidyr::replace_na(individuals, 1),
-                individuals = ifelse(individuals > 0, 1, 1))
+    here::here("dados", "tabelas", "ATLANTIC_AMPHIBIANS_species.csv"), 
+    col_types = cols()) %>%
+    tidyr::drop_na(valid_name) %>% 
+    dplyr::select(id, valid_name, individuals) %>% 
+    dplyr::distinct(id, valid_name, .keep_all = TRUE) %>% 
+    dplyr::mutate(individuals = tidyr::replace_na(individuals, 1),
+                  individuals = ifelse(individuals > 0, 1, 1))
 
 ## Download do Bioma da Mata Atlântica
 geo_vetor_mata_atlantica <- geobr::read_biomes(year = 2019, showProgress = FALSE) %>% 
-  dplyr::filter(name_biome == "Mata Atlântica") %>% 
-  sf::st_transform(crs = 4326) %>% 
-  sf::st_crop(xmin = -55, ymin = -30, xmax = -34, ymax = -5)
+    dplyr::filter(name_biome == "Mata Atlântica") %>% 
+    sf::st_transform(crs = 4326) %>% 
+    sf::st_crop(xmin = -55, ymin = -30, xmax = -34, ymax = -5)
 
 ## Aumentar o tempo de download
 options(timeout = 1e5)
 
-## Download dos dados do GlobCover
-download.file(url = "http://due.esrin.esa.int/files/Globcover2009_V2.3_Global_.zip",
-              destfile = here::here("dados", "raster", "Globcover2009_V2.3_Global_.zip"), 
-              mode = "wb", extra = "c")
-
-## Unzip
-unzip(zipfile = here::here("dados", "raster", "Globcover2009_V2.3_Global_.zip"),
-      exdir = here::here("dados", "raster"))
-
 ## Importar raster do GlobCover
-geo_raster_globcover <- raster::raster(
-  here::here("dados", "raster", "GLOBCOVER_L4_200901_200912_V2.3.tif"))
+geo_raster_globcover_mata_atlantica <- raster::raster(
+    here::here("dados", "raster", "geo_raster_globcover_mata_atlantica.tif"))
 
+saveRDS(geo_anfibios_locais, "geo_anfibios_locais.rds")                
+saveRDS(geo_raster_bioclim, "geo_raster_bioclim.rds")                  
+saveRDS(geo_raster_globcover_mata_atlantica, "geo_raster_globcover_mata_atlantica.rds")
+saveRDS(geo_raster_srtm, "geo_raster_srtm.rds")         
+saveRDS(geo_vetor_am_sul, "geo_vetor_am_sul.rds")                   
+saveRDS(geo_vetor_biomas, "geo_vetor_biomas.rds")                 
+saveRDS(geo_vetor_brasil_anos, "geo_vetor_brasil_anos.rds")           
+saveRDS(geo_vetor_brasil, "geo_vetor_brasil.rds")                
+saveRDS(geo_vetor_cobertura, "geo_vetor_cobertura.rds")                
+saveRDS(geo_vetor_hidrografia, "geo_vetor_hidrografia.rds")          
+saveRDS(geo_vetor_mata_atlantica, "geo_vetor_mata_atlantica.rds")           
+saveRDS(geo_vetor_nascentes, "geo_vetor_nascentes.rds")           
+saveRDS(geo_vetor_rio_claro, "geo_vetor_rio_claro.rds")
 
-## Baixas os dados
-geo_dados_agua <- sidrar::get_sidra(x = 3218,
-                                    variable = 1000096,
-                                    period = "last",
-                                    classific = c("c61"),
-                                    geo = "City")
-
-geo_dados_banheiro <- sidrar::get_sidra(x = 3218,
-                                        variable = 1000096,
-                                        period = "last",
-                                        classific = c("c299"),
-                                        geo = "City")
-
-geo_dados_lixo  <- sidrar::get_sidra(x = 3218,
-                                     variable = 1000096,
-                                     period = "last",
-                                     classific = c("c67"),
-                                     geo = "City")
-
-geo_dados_luz <- sidrar::get_sidra(x = 3218,
-                                   variable = 1000096,
-                                   period = "last",
-                                   classific = c("c309"),
-                                   geo = "City")
-
-## Vetor
-geo_vetor_brasil_municipios <- geobr::read_municipality(
-  code_muni = "all", year = 2010, showProgress = FALSE)
-
+readRDS("geo_anfibios_locais.rds")                
+readRDS("geo_raster_bioclim.rds")                  
+readRDS("geo_raster_globcover_mata_atlantica.rds")
+readRDS("geo_raster_srtm.rds")         
+readRDS("geo_vetor_am_sul.rds")                   
+readRDS("geo_vetor_biomas.rds")                 
+readRDS("geo_vetor_brasil_anos.rds")           
+readRDS("geo_vetor_brasil.rds")                
+readRDS("geo_vetor_cobertura.rds")                
+readRDS("geo_vetor_hidrografia.rds")          
+readRDS("geo_vetor_mata_atlantica.rds")           
+readRDS("geo_vetor_nascentes.rds")           
+readRDS("geo_vetor_rio_claro.rds")
